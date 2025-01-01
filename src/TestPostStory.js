b1294160ad8cb0876c3e1ef4b53c671cf6ee1bc5
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-01-01 11:50:28 +0700

    test: Life cycle test

--- src/TestPostStory.js
+++ src/TestPostStory.js
@@ -1,12 +1,23 @@
 import http from 'k6/http';
 import exec from 'k6/execution';
+import { check } from 'k6';
 
 export const options = {
   vus: 10,
   duration: '10s'
 };
 
-export default function() {
+export function setup() {
+  const data = [];
+  for (let i = 0; i < 10; i++) {
+    data.push({
+      description: `Story-${i}`
+    });
+  }
+  return data;
+}
+
+export function getToken() {
   const email = `example${exec.vu.idInInstance}@gmail.com`;
   
   const requestLogin = {
@@ -21,17 +32,25 @@ export default function() {
     }
   });
 
-  const token = `Bearer ${responseLogin.json().loginResult.token}`
+  return `Bearer ${responseLogin.json().loginResult.token}`
+}
 
-  const requestPostStory = {
-    description: "lorem ipsum dolor sit amet"
+export default function(data) {
+  const token = getToken();
+
+  for (let i = 0; i < data.length; i++) {
+    const story = data[i];
+    const responsePost = http.post("http://localhost:3000/stories", JSON.stringify(story), {
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": token
+      }
+    });
+
+    check(responsePost, {
+      'create status was 200': (res) => res.status === 201
+    })
   }
-  
-  http.post("http://localhost:3000/stories", JSON.stringify(requestPostStory), {
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": token
-    }
-  })
+
 }

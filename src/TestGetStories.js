80c9173e4c0e8e1a0aee8d076f4f11326760a63c
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-01-01 09:03:31 +0700

    test: Check and fail functions test

--- src/TestGetStories.js
+++ src/TestGetStories.js
@@ -9,11 +9,12 @@ export const options = {
 export default function() {
     const uniqEmail = new Date().getTime();
 
+    // Register
     const requestRegister = {
         name: "Test Name",
         email: `${uniqEmail}@gmail.com`,
         password: "11112222"
-    }
+    };
 
     const responseRegister = http.post("http://localhost:3000/register", JSON.stringify(requestRegister), {
         headers: {
@@ -22,10 +23,20 @@ export default function() {
         }
     });
 
+    const checkRegister = check(responseRegister, {
+        'response code was 200': (res) => res.status == 201,
+        'user was created': (res) => res.json().message === "User created",
+    });
+
+    if (!checkRegister) {
+        fail(`Failed to register user ${uniqEmail}@gmail.com`);
+    }
+
+    // Login
     const requestLogin = {
         email: `${uniqEmail}@gmail.com`,
         password: "11112222"
-    }
+    };
 
     const responseLogin = http.post("http://localhost:3000/login", JSON.stringify(requestLogin), {
         headers: {
@@ -34,14 +45,31 @@ export default function() {
         }
     });
 
-    const token = `Bearer ${responseLogin.json().loginResult.token}`
+    const checkLogin = check(responseLogin, {
+        'login response code was 200': (res) => res.status === 200,
+        'login response token was exist': (res) => res.json().loginResult?.token != null
+    });
 
-    http.get("http://localhost:3000/stories", {
+    if (!checkLogin) {
+        fail(`Failed to login user ${uniqEmail}@gmail.com`);
+    }
+
+    // Get Stories
+    const token = `Bearer ${responseLogin.json().loginResult.token}`;
+
+    const responseStories = http.get("http://localhost:3000/stories", {
         headers: {
             "Accept": "application/json",
             "Authorization": token
         }
     });
 
-    
+    const checkStory = check(responseStories, {
+        'story response code was 200': (res) => res.status === 200,
+        'story was exist': (res) => res.json().listStory != null
+    });
+
+    if (!checkStory) {
+        fail(`Failed to fetch story`);
+    }
 }
